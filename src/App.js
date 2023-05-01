489033efd3410d6c3a5d54904ee95998d555f9cf
chaoticevil7 <solopovav@icloud.com>
2023-05-01 16:54:06 +0300

    clean state

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import React from 'react';
-//import VideoBackground from './components/VideoBackground'; 
+import VideoBackground from './components/VideoBackground'; 
 import './style.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <VideoBackground /> */}
-      <div className="content">
-        <div className="animation">
-          <div className="intro">
-            <h1>FUND</h1>
-            <h2>CHAIN</h2>
-            <video src="/ball.mp4"></video>
-          </div> 
-        </div>
-        <div className="rectangle">
-          <div className="connect-wallet">CONNECT WALLET</div>
-        </div>
-        <div className="success">
-          SUCCESS TRANSPARENTLY
-        </div>
-        <section>
-          <h1>MYBALL</h1>
-        </section>
-        <div className="new-element">
-          FUNDCHAIN AIMS TO BE DECENTRALIZED CROWDFUNDING PLATFORM BASED ON SMART CONTRACTS, NO CENTRAL AUTHORITY.
-        </div>
-        <div className="support-transparently">
-          <p>SUPPORT TRANSPARENTLY</p>
-        </div>  
-      </div>
+      <VideoBackground />
     </div>
   );
 }
-
-// const controller = new ScrollMagic.Controller();
-
-// const scene1 = new ScrollMagic.Scene({
-//   triggerElement: '.animation',
-//   duration: '100%'
-// })
-// .setTween('.video-container video', {
-//   opacity: 0,
-//   duration: 2
-// })
-// .addTo(controller);
-
 
 export default App;
